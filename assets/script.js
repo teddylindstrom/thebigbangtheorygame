e0aaef27ad11b733a36bb0606b1a1747828b2382
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-06-19 21:27:58 +0000

    added some JS to the script but I have to wait to expand it until tomorrow morning when I've had my meeting with my mentor because i don't understand it

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+const playerChoiceDisplay = document.getElementById("player-choice");
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const choices = ["rock", "paper", "scissors"];
+let computerChoice;
