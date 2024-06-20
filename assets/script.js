b9441a49904e12c38c14ee569da7afbfd7240018
Teddy Lindström <teddy.lindstrom.work@gmail.com>
2024-06-20 18:52:22 +0000

    added some start of function

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
+// Wait for the DOM to finish loading before running the game
+// Add event listeners to the buttons
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    
+
 const playerChoiceDisplay = document.getElementById("player-choice");
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const choices = ["rock", "paper", "scissors"];
